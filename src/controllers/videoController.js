f06b371067b5aa25dcb8690fc3f37af9bc6aae54
Jo  Seongyun <whtjddbs0112@naver.com>
2023-04-21 22:24:11 +0900

    s3 uploads

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -71,8 +71,9 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].path,
+      fileUrl: video[0].location,
       thumbUrl: `/${thumb[0].destination}${thumb[0].filename}`,
+      // thumbUrl: thumb[0].location,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
